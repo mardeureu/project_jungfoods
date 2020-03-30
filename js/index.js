1e9e9f935d4e09971ce3c99ae3dc686693fbd176
study <siixx@hamail.net>
2020-03-30 19:36:48 +0900

    test 1
    .

---
+++ js/index.js
@@ -0,0 +1,21 @@
+window.addEventListener('click',function(){
+    var idx = 0;
+    
+});
+
+/*
+
+product
+탭 눌렀을 때
+
+상품 5개
+
+왼쪽 > 오른쪽
+
+0.2 초 딜레이 순차적으로 나온다
+
+슬라이드 없음
+
+오버레이? 자연스럽게 사라졌다 나타난다.
+
+*/
