91e05c53fe5abb3a8e2547009248e342638c60e1
study <siixx@hamail.net>
2020-04-14 17:04:16 +0900

    /

--- js/header.js
+++ js/header.js
@@ -1,26 +1,22 @@
-// 작성자 : 정선희
-
-/* 버거메뉴 클릭*/
 function burgerMenuBtn() {
     if (globalNav.className == "menuVisible") {
         globalNav.className = ""
     } else {
         globalNav.className = "menuVisible"
     }
-}//END
+}
 
 window.addEventListener('load',function(){
     var idx = 0;    
     var mainMenu = document.querySelectorAll('.main-menu > li a');
+    
     for(let i = 0; i <= mainMenu.length-1; i++){
         mainMenu[i].onclick = function (){
             mainMenu[idx].className = "";
             this.className = "subMenuVisivble";
             idx = i;
-        }//onclick func END
+        }
+        console.log(mainMenu[idx]);
     }
-});//window onload func END
-
-
-// 스크롤, top 버튼, 메인 제품 
-// ㅠㅠ...
+    console.log("for 밖 : "+mainMenu[idx]);
+});
