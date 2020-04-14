04f1b71ab6c8ba9c3737beabcfe1f5ae2ec890a0
study <siixx@hamail.net>
2020-04-14 12:05:26 +0900

    header 수정

--- js/header.js
+++ js/header.js
@@ -9,20 +9,18 @@ function burgerMenuBtn() {
     }
 }//END
 
-window.onload = function(){
+window.addEventListener('load',function(){
+    var idx = 0;    
+    var mainMenu = document.querySelectorAll('.main-menu > li a');
+    for(let i = 0; i <= mainMenu.length-1; i++){
+        mainMenu[i].onclick = function (){
+            mainMenu[idx].className = "";
+            this.className = "subMenuVisivble";
+            idx = i;
+        }//onclick func END
+    }
+});//window onload func END
 
-    var mainMenu = document.querySelectorAll('.main-menu li a');
-    mainMenu.onclick = function (){
-       
-        for(var i = 0; i >= mainMenu.length-1; i++){
-            mainMenu[i];
 
-        }
-    }//onclick func END
-
-}//window onload func END
-
-
-// 스크롤, top 버튼, 메인 제품, 헤더 커졌을 때 감지 못하는것, height 스크롤 하단 못찾는것.
-// 버거 메뉴 서브 호버 할 때 펼쳐 지는 것...
+// 스크롤, top 버튼, 메인 제품 
 // ㅠㅠ...
