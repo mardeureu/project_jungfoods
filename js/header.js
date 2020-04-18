7d76cf1abf6c4e23fea5fbcbf0978834f830437a
mardeureu <siixx@hanmail.net>
2020-04-19 02:16:00 +0900

    /

--- js/header.js
+++ js/header.js
@@ -1,12 +1,48 @@
-function burgerMenuBtn() {
+function MenuBtn(breadCrust) {
     if (globalNav.className == "menuVisible") {
-        globalNav.className = ""
+        globalNav.className = "";
     } else {
-        globalNav.className = "menuVisible"
+        globalNav.className = "menuVisible";
     }
 }
-
 window.addEventListener('load',function(){
+    //bread_crust animate
+    var burgerMenuBtn = document.getElementById('burgerMenuBtn');
+    var bread_crust_top = burgerMenuBtn.children[0];
+    var bread_crust_bottom = burgerMenuBtn.children[1];
+    var bread_crust_top_span = bread_crust_top.children[0];
+    var bread_crust_bottom_span = bread_crust_bottom.children[0];
+    burgerMenuBtn.addEventListener('click',function(){
+        if (bread_crust_top.className == "bread-crust-top") {
+            bread_crust_top.className = "";
+        } else {
+            bread_crust_top.className = "bread-crust-top";
+        }
+    });
+    burgerMenuBtn.addEventListener('click',function(){
+        if (bread_crust_bottom.className == "bread-crust-bottom") {
+            bread_crust_bottom.className = "";
+        } else {
+            bread_crust_bottom.className = "bread-crust-bottom";
+        }
+    });
+    //X
+    burgerMenuBtn.addEventListener('click',function(){
+        if (bread_crust_top.className == "bread-crust-top"){
+            bread_crust_top_span.style.top = "50%";
+        } else {
+            bread_crust_top_span.style.top =  "calc(50% - 5px)";
+        }
+    });
+    burgerMenuBtn.addEventListener('click',function(){
+        if (bread_crust_bottom.className == "bread-crust-bottom"){
+            bread_crust_bottom_span.style.top = "50%";
+        } else {
+            bread_crust_bottom_span.style.top =  "calc(50% + 5px)";
+        }
+    });
+
+    //sub menu
     var idx = 0;    
     var mainMenu = document.querySelectorAll('.main-menu > li a');
     
@@ -16,7 +52,5 @@ window.addEventListener('load',function(){
             this.className = "subMenuVisivble";
             idx = i;
         }
-        console.log(mainMenu[idx]);
     }
-    console.log("for 밖 : "+mainMenu[idx]);
 });
